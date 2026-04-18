25afd4221659f41368c5bf8147a001e7b6de57d3
John Ades <john.a.ades@gmail.com>
2026-04-18 12:18:34 -0400

    fixed eval launch

--- lighteval/eval.sh
+++ lighteval/eval.sh
@@ -5,7 +5,7 @@ uv pip install word2number
 
 #lighteval vllm \
 #"model_name=$1,revision=main,dtype=bfloat16,tensor_parallel_size=8" \
-uv accelerate launch -m lighteval accelerate \
+uv run accelerate launch -m lighteval accelerate \
 "model_name=$1,revision=main,dtype=bfloat16,max_length=2048,trust_remote_code=True" \
 "custom|hellaswag|0|1,custom|arc|0|1,custom|piqa|0|1,custom|winogrande|0|1,custom|openbook_qa|0|1,custom|mmlu|0|1" \
 --custom-tasks "tasks.py"
